44bc7086a714686964ce7347b485c3738a53bd6a
ST-GuY <estebanlvy23@gmail.com>
2025-07-22 14:25:09 +0200

    test

--- main.h
+++ main.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+#include <stdarg.h>
 
+typedef struct type
+{
+	char letter;
+	void (*p)(va_list);
+} type_t;
+
+int _printf(const char *format, ...);
 
 #endif
